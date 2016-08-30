86a3235e45d4bdf572d69b2b3f542862d49f7a0c
Zoey Bendef <zbendef@gmail.com>
2016-08-30 15:38:51 +1000

    NEW: Create high score view model for binding player scores

---
+++ Compound/ViewModels/HighScoreViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using System.Collections.ObjectModel;
+
+namespace Compound
+{
+	public class HighScoreViewModel
+	{
+		public ObservableCollection<Score> HighScoresList { get; set; }
+
+		public HighScoreViewModel()
+		{
+			this.HighScoresList = new ObservableCollection<Score>(
+				new Score[] {
+				new Score("Zoey", 100),
+				new Score("Amy", 95)
+			});
+		}
+	}
+}
+
