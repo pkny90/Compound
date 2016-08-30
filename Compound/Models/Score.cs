4e81d178d56c379dddaab4ee426b56eff6dfc382
Zoey Bendef <zbendef@gmail.com>
2016-08-30 15:38:27 +1000

    NEW: Create model for high scores

---
+++ Compound/Models/Score.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace Compound
+{
+	public class Score
+	{
+		public string PlayerName { get; private set; }
+		public int PlayerScore { get; private set; }
+
+		public Score(string name, int score)
+		{
+			this.PlayerName = name;
+			this.PlayerScore = score;
+		}
+
+		public override string ToString()
+		{
+			return string.Format("{0}\t{1}", PlayerName, PlayerScore);
+		}
+	}
+}
+
