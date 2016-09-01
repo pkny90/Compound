52dac44f6c367415ca62202476349278d61b2c38
RenruiLiu <liur@deakin.edu.au>
2016-09-01 22:02:20 +1000

    NEW: Create main menu page
    Added buttons on MainMenuPage and when click play button, it will pop up
    a dialog for user to choose difficulty. And created an empty about page

--- Compound/Views/MainMenuPage.xaml.cs
+++ Compound/Views/MainMenuPage.xaml.cs
@@ -1,20 +1,40 @@
 ﻿using System;
+using System.Diagnostics;
 using Xamarin.Forms;
 
 namespace Compound
 {
 	public partial class MainMenuPage : ContentPage
 	{
+		
 		public MainMenuPage()
 		{
 			InitializeComponent();
 			NavigationPage.SetHasNavigationBar(this, false);
+			SizeChanged += OnSizeChanged;		
 		}
 
-		public void GoToHighScorePage(object sender, EventArgs e) 
+		void OnSizeChanged(object sender, EventArgs e)
+		{
+			
+		}
+
+		public void GoToHighScorePage(object sender, EventArgs e)
 		{
 			var highscorePage = new HighScorePage();
 			Navigation.PushAsync(highscorePage);
+		}
+		public void GoToAboutPage(object sender, EventArgs e)
+		{
+			var aboutPage = new AboutPage();
+			Navigation.PushAsync(aboutPage);
+		}
+
+		async void ChooseDifficulty(object sender, EventArgs e)
+		{
+			var action = await DisplayActionSheet("Please select a difficulty:", "Cancel", null, "Easy", "Medium", "Hard");
+			Debug.WriteLine("Action: " + action);
+
 		}
 	}
 }
