83d49a81da75a59680aca2f4cb6abd3c78a63d22
Zoey Bendef <zbendef@gmail.com>
2016-09-17 20:18:35 +1000

    NEW: Make model for game words

---
+++ Compound/Models/Word.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Compound
+{
+	public class Word
+	{
+		public string word { get; set; }
+		public string first_image { get; set; }
+		public string second_image { get; set; }
+		public string answer_image { get; set; }
+		public string first_word { get; set; }
+		public string second_word { get; set; }
+
+		public Word(string word, string firstImage, string secondImage,
+		           string answerImage, string firstWord, string secondWord)
+		{
+			this.word = word;
+			this.first_image = firstImage;
+			this.second_image = secondImage;
+			this.answer_image = answerImage;
+			this.first_word = firstWord;
+			this.second_word = secondWord;
+		}
+	}
+}
+
