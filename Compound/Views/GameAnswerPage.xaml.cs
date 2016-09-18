16a5c18ba61f891e1b83340ae1651f48fcc0f683
Zoey Bendef <zbendef@gmail.com>
2016-09-18 20:34:54 +1000

    NEW: Create game answer submitted page

---
+++ Compound/Views/GameAnswerPage.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace Compound
+{
+	public partial class GameAnswerPage : ContentPage
+	{
+		public GameAnswerPage()
+		{
+			InitializeComponent();
+		}
+	}
+}
+
